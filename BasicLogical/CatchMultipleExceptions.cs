b3bcdf299e528c52a3582294552c01d0a49317e0
pranayborode <pranayborode@gmail.com>
2024-07-21 17:52:23 +0530

    CatchMultipleExceptions

--- BasicLogical/CatchMultipleExceptions.cs
+++ BasicLogical/CatchMultipleExceptions.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicalPractice
+namespace LogicalPractice.BasicLogical
 {
-	internal class Program
+	internal class CatchMultipleExceptions
 	{
-	
 
 		public static void CatchExceptions()
 		{
@@ -17,20 +16,19 @@ namespace LogicalPractice
 				int[] num = { 1, 2, 3 };
 				Console.WriteLine(num[4]); // This will throw an IndexOutOfRangeException
 			}
-			catch(Exception ex) when (ex is IndexOutOfRangeException || ex is ArithmeticException)
+			catch (Exception ex) when (ex is IndexOutOfRangeException || ex is ArithmeticException)
 			{
-                Console.WriteLine("Handled IndexOutOfRangeException or ArithmeticaException");
-            }
+				Console.WriteLine("Handled IndexOutOfRangeException or ArithmeticaException");
+			}
 			catch (Exception ex)
 			{
 				Console.WriteLine($"Handled {ex.GetType()} exception");
 			}
 		}
 
-		static void Main(string[] args)
-		{
-			CatchExceptions();
-		}
-			
+		//static void Main(string[] args)
+		//{
+		//	CatchExceptions();
+		//}
 	}
 }
