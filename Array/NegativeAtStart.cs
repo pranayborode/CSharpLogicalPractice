c58f9562a4387ddbb5aa75659c9f377066d773e0
pranayborode <pranayborode@gmail.com>
2024-07-31 21:49:10 +0530

    ArrayLogical

--- Array/NegativeAtStart.cs
+++ Array/NegativeAtStart.cs
@@ -8,5 +8,38 @@ namespace LogicalPractice.Array
 {
 	internal class NegativeAtStart
 	{
+		public static void PrintNegativeAtStart(int[] arr)
+		{
+			int size = arr.Length;
+			for (int i = 0; i < size; i++)
+			{
+				if (arr[i] < 0)
+				{
+					continue;
+				}
+				else
+				{
+					for (int j = size - 1; j >= 0 && i < j; j--)
+					{
+						if (arr[j] < 0)
+						{
+							int temp = arr[i];
+							arr[i] = arr[j];
+							arr[j] = temp;
+							break;
+						}
+					}
+				}
+			}
+			foreach (int i in arr)
+			{
+				Console.Write(i + " ");
+			}
+		}
+		//static void Main(string[] args)
+		//{
+		//	int[] arr = { 1, -2, 3, -4, 0, -5 };
+		//	PrintNegativeAtStart(arr);
+		//}
 	}
 }
