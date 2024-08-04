f931d89ed6d0502eadc35eda0bef4c511b25933b
pranayborode <pranayborode@gmail.com>
2024-08-04 16:36:55 +0530

    Generic

---
+++ Generic/DictionaryEmployeeExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPractice.Generic
+{
+	public class emp
+	{
+		private string name;
+		private int salary;
+
+		public emp(string name, int salary)
+		{
+			this.name = name;
+			this.salary = salary;
+		}
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder(200);
+			sb.AppendFormat("{0},{1}", name, salary);
+
+			return sb.ToString();
+		}
+
+	}
+	internal class DictionaryEmployeeExample
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, emp> dObj = new Dictionary<string, emp>(2);
+
+			emp tom = new emp("tom", 2000);
+			dObj.Add("tom", tom);   // key,value
+			emp john = new emp("john", 4000);
+			dObj.Add("john", john);
+
+			foreach (Object str in dObj.Values)
+			{
+				Console.WriteLine(str);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Generic/DictionaryExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPractice.Generic
+{
+	internal class DictionaryExample
+	{
+		//static void Main(string[] args)
+		//{
+		//	Dictionary<int, string> dobj = new Dictionary<int, string>(5);
+
+		//	dobj.Add(1, "One");
+		//	dobj.Add(2, "Two");
+		//	dobj.Add(3, "Three");
+		//	dobj.Add(4, "Four");
+		//	dobj.Add(5, "Five");
+
+		//	for (int i = 1; i <= dobj.Count; i++)
+		//	{
+		//		Console.WriteLine(dobj[i]);
+		//	}
+		//	Console.ReadKey();
+		//}
+	}
+}
