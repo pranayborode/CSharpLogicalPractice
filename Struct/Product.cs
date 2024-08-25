67ba18e7c30a66df183900fd5a2f3c59c994653c
pranayborode <pranayborode@gmail.com>
2024-08-26 01:49:08 +0530

    gtiUpdate

---
+++ Struct/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPractice.Struct
+{
+	internal struct Product
+	{
+		private int id;
+        private int price;
+        private int total;
+       
+        public static void CalcTotal()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+         
+    public class Shop : Product
+    {
+        
+    }
+}
